991e8b24229f0dba372aca0e022702e4e9748ac0
konradkrasocki <konrad.krasocki@gmail.com>
2023-08-16 08:28:39 +0200

    api key private

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict";
+import { api_token } from "./config";
 
 // Selectors:
 // Setions:
@@ -207,7 +208,7 @@ const slider = function () {
 
 // Toggl API hours spent programming
 const togglAPI = function () {
-  const apiToken = "f4d69d308e97e4bf700051591f16876f";
+  const apiToken = api_token;
   const url = "https://api.track.toggl.com/api/v9/me?with_related_data=true";
 
   async function getTimeLearning(apiToken) {
