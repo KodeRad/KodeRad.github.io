9ca3bcd0889e1d560b959b4a3a2ac57656156cc1
konradkrasocki <konrad.krasocki@gmail.com>
2023-10-10 21:34:48 +0200

    api key secret

--- script.js
+++ script.js
@@ -206,45 +206,21 @@ const slider = function () {
 ///////////////////
 
 // Toggl API hours spent programming
-// const togglAPI = function () {
-//   const apiKey = "";
-//   const url = "https://api.track.toggl.com/api/v9/me?with_related_data=true";
-
-//   async function getTimeLearning() {
-//     try {
-//       const response = await fetch(url, {
-//         method: "GET",
-//         headers: {
-//           "Content-Type": "application/json",
-//           Authorization: `Basic ${btoa(`${apiKey}:api_token`)}`,
-//         },
-//       });
-//       const data = await response.json();
-//       const hours = data.projects[8].actual_hours;
-
-//       // DOM Manipulation
-//       document.getElementById(
-//         "skills_description"
-//       ).textContent = `I have spent ${hours} hours (current time form API!) learning mainly JavaScript along with
-//       HTML and CSS. Thanks to that commitment I believe I will bring a lot
-//       to the table if we ever met business-wise.`;
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
-//   getTimeLearning();
-// };
-
-// Toggl API hours spent programming
-const getTime = function () {
-  const url = "https://koderad-api.onrender.com/toggl";
+const togglAPI = function () {
+  const apiKey = secrets.TOGGL_API_KEY;
+  const url = "https://api.track.toggl.com/api/v9/me?with_related_data=true";
 
   async function getTimeLearning() {
     try {
       const response = await fetch(url, {
         method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Basic ${btoa(`${apiKey}:api_token`)}`,
+        },
       });
-      const hours = await response.json();
+      const data = await response.json();
+      const hours = data.projects[8].actual_hours;
 
       // DOM Manipulation
       document.getElementById(
@@ -258,6 +234,30 @@ const getTime = function () {
   }
   getTimeLearning();
 };
+
+// Toggl API hours spent programming
+// const getTime = function () {
+//   const url = "https://koderad-api.onrender.com/toggl";
+
+//   async function getTimeLearning() {
+//     try {
+//       const response = await fetch(url, {
+//         method: "GET",
+//       });
+//       const hours = await response.json();
+
+//       // DOM Manipulation
+//       document.getElementById(
+//         "skills_description"
+//       ).textContent = `I have spent ${hours} hours (current time form API!) learning mainly JavaScript along with
+//       HTML and CSS. Thanks to that commitment I believe I will bring a lot
+//       to the table if we ever met business-wise.`;
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+//   getTimeLearning();
+// };
 
 ///////////////////////////////////
 
