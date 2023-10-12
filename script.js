c4efdc9c5a445f48045a297a754b23a84a47580e
konradkrasocki <konrad.krasocki@gmail.com>
2023-10-12 13:02:42 +0200

    github action

--- script.js
+++ script.js
@@ -207,7 +207,7 @@ const slider = function () {
 
 // Toggl API hours spent programming
 const togglAPI = function () {
-  const apiKey = secrets.TOGGL_API_KEY;
+  const apiKey = TOGGL_API_KEY;
   const url = "https://api.track.toggl.com/api/v9/me?with_related_data=true";
 
   async function getTimeLearning() {
