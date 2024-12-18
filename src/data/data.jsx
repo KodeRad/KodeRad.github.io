9c0b60beeb9624939da2cdd81ae51bdcd77eb61f
Konrad Krasocki <104936644+KodeRad@users.noreply.github.com>
2024-12-18 21:21:25 +0100

    Init commit

---
+++ src/data/data.jsx
@@ -0,0 +1,61 @@
+// src/data.js
+
+export const projects = [
+  {
+    title: 'React Reserve',
+    subtitle: 'MERN Stack',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.5_tuR5G6F3h0GhZeTB9GQAAAAA%26pid%3DApi&f=1&ipt=41b3f875f9a3455f6e6fc1f737027f57916b958425ee5221359a8bcc1067ca46&ipo=images',
+    link: 'https://reactbootcamp.com',
+  },
+  {
+    title: 'React Tracks',
+    subtitle: 'React and Python',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP._6DPkK3D9UpQHgF-2zZRIAAAAA%26pid%3DApi&f=1&ipt=51acc5256916f2fc7be8a0fd5e9482c25ccb9490e268c55d5aabc87fd0322017&ipo=images',
+    link: 'https://reedbarger.com',
+  },
+  {
+    title: 'DevChat',
+    subtitle: 'React and Firebase',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.NpOFFI3zksbDrvBEJbs0GgAAAA%26pid%3DApi&f=1&ipt=f853199ae0673546371a680dc413431b070b13105ff8fce3589b684e82144971&ipo=images',
+    link: 'https://jsbootcamp.com',
+  },
+  {
+    title: 'Epic Todo App',
+    subtitle: 'React Hooks',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    image:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.6joz1ifbEe-uE8ASc9t2KQAAAA%26pid%3DApi&f=1&ipt=b6357d685fb05fc70aa9eea09073cfe899538724c2d89af96fa7384e2e22cfb5&ipo=images',
+    link: 'https://pythonbootcamp.com',
+  },
+];
+
+export const skills = ['react', 'java', 'javascript', 'docker', 'html', 'css'];
+
+export const testimonials = [
+  {
+    name: 'Rafal',
+    company: 'Smartbear',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius',
+    image:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.v-soxWBIJRUBFhKwt-kfiwAAAA%26pid%3DApi&f=1&ipt=c5477fcef279eb479f6c0dc2fa6a6233bfeea0c71d7711fedb620492674d3104&ipo=images',
+  },
+  {
+    name: 'Joanna',
+    company: 'Smartbear',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius',
+    image:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.explicit.bing.net%2Fth%3Fid%3DOIP.R8m0ojgAiuCEclGS9eVSBAAAAA%26pid%3DApi&f=1&ipt=6b7d6d0c67eeb51df841a71e0750af94a5e8613e0c4da7db049cb629f6591944&ipo=images',
+  },
+];
