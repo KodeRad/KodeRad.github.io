7b0389f80dcb9935e2410d9e690c68f15bf2d438
KodeRad <konrad.krasocki@gmail.com>
2025-04-26 17:02:57 +0200

    smooth scrolling

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,64 +1,64 @@
 // src/data.js
 
 export const projects = [
-  {
-    title: 'My portfolio website',
-    subtitle: 'Vanilla JS',
-    description:
-      'The first page that I have created in my career. I thought I would become a Frontend Dev, but it turned out I am coding mostly Java now.',
-    image: '/src/assets/my-portfolio.png',
-    link: 'https://koderad.github.io/',
-  },
-  {
-    title: 'Dodone - todo list - frontend',
-    subtitle: 'React + Material-UI',
-    description:
-      'A simple Todo List application built with React, Vite, and Material-UI.',
-    image: 'src/assets/dodone.png',
-    link: 'https://github.com/KodeRad/dodone-front',
-  },
-  {
-    title: 'Dodone - todo list - backend',
-    subtitle: 'Java + Spring Boot + mySQL',
-    description: 'Spring Boot Rest API service',
-    image: 'src/assets/dodone-back.png',
-    link: 'https://github.com/KodeRad/dodone-back',
-  },
-  {
-    title: 'Epic Todo App',
-    subtitle: 'React Hooks',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-    image:
-      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.6joz1ifbEe-uE8ASc9t2KQAAAA%26pid%3DApi&f=1&ipt=b6357d685fb05fc70aa9eea09073cfe899538724c2d89af96fa7384e2e22cfb5&ipo=images',
-    link: 'https://pythonbootcamp.com',
-  },
+    {
+        title: 'My portfolio website',
+        subtitle: 'Vanilla JS',
+        description:
+            'The first page that I have created in my career. I thought I would become a Frontend Dev, but it turned out I am coding mostly Java now.',
+        image: '/src/assets/my-portfolio.png',
+        link: 'https://koderad.github.io/old-portfolio-site/',
+    },
+    {
+        title: 'Dodone - todo list - frontend',
+        subtitle: 'React + Material-UI',
+        description:
+            'A simple Todo List application built with React, Vite, and Material-UI.',
+        image: 'src/assets/dodone.png',
+        link: 'https://github.com/KodeRad/dodone-front',
+    },
+    {
+        title: 'Dodone - todo list - backend',
+        subtitle: 'Java + Spring Boot + mySQL',
+        description: 'Spring Boot Rest API service',
+        image: 'src/assets/dodone-back.png',
+        link: 'https://github.com/KodeRad/dodone-back',
+    },
+    {
+        title: 'Nowy Młyn - Koło Natury',
+        subtitle: 'Business landing page',
+        description:
+            'Agrotourism website for a small family business. Work in progress.',
+        image:
+            'https://i.postimg.cc/jjX99nqZ/kolo-natury.png',
+        link: 'https://koderad.github.io/kolo-natury/',
+    },
 ];
 
 export const skills = [
-  'JAVA',
-  'REACT',
-  'JAVASCRIPT',
-  'DOCKER',
-  'REST API',
-  'CSS',
+    'JAVA',
+    'REACT',
+    'JAVASCRIPT',
+    'DOCKER',
+    'REST API',
+    'CSS',
 ];
 
 export const testimonials = [
-  {
-    name: 'Rafal',
-    company: 'Smartbear',
-    quote:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius',
-    image:
-      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.v-soxWBIJRUBFhKwt-kfiwAAAA%26pid%3DApi&f=1&ipt=c5477fcef279eb479f6c0dc2fa6a6233bfeea0c71d7711fedb620492674d3104&ipo=images',
-  },
-  {
-    name: 'Joanna',
-    company: 'Smartbear',
-    quote:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius',
-    image:
-      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.explicit.bing.net%2Fth%3Fid%3DOIP.R8m0ojgAiuCEclGS9eVSBAAAAA%26pid%3DApi&f=1&ipt=6b7d6d0c67eeb51df841a71e0750af94a5e8613e0c4da7db049cb629f6591944&ipo=images',
-  },
+    {
+        name: 'Aleksander',
+        company: 'Smartbear',
+        quote:
+            'Konrad is a great developer. He is very helpful and always willing to help others. He is a great team player and a great person to work with.',
+        image:
+            'https://ca.slack-edge.com/E029SJ55Y4T-U05UQV23B0E-cd8eb835ae12-512'
+    },
+    {
+        name: 'Joanna',
+        company: 'Smartbear',
+        quote:
+            'Konrad struck me as a very talented and hard-working person. He is very helpful and always willing to help others. He is a great team player and a great person to work with.',
+        image:
+            'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.explicit.bing.net%2Fth%3Fid%3DOIP.R8m0ojgAiuCEclGS9eVSBAAAAA%26pid%3DApi&f=1&ipt=6b7d6d0c67eeb51df841a71e0750af94a5e8613e0c4da7db049cb629f6591944&ipo=images',
+    },
 ];
---
+++ src/helpers/HelperFunctions.js
@@ -0,0 +1,8 @@
+export const handleSmoothScroll = (e) => {
+    e.preventDefault();
+    const targetId = e.currentTarget.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+};
